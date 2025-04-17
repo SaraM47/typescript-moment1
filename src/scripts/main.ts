bb27af67dff96b2dd1226a2ae47b425184c884c5
SaraMsoso <Sara.soder44@gmail.com>
2025-04-17 13:00:29 +0200

    Adding DOM element references and function to load courses from localStorage

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,7 +1,28 @@
+// Definierar en typ för kursernas data. Varje kurs bör ha dessa fyra egenskaper.
 interface CourseInfo {
     code: string;
     name: string;
     progression: 'A' | 'B' | 'C';
     syllabus: string;
+  }
+  
+  /* 
+  * Hämtar referenser till HTML-element vi ska använda i skriptet
+  * Sedan konverterar text till JavaScript-objekt och sluligen visar kurserna på sidan
+  */
+  const courseForm = document.getElementById('course-form') as HTMLFormElement;
+  const courseList = document.getElementById('course-list') as HTMLUListElement;
+  const filterInput = document.getElementById('filter') as HTMLInputElement;
+  
+  // Skapar en tom lista som kommer innehålla alla kurser
+  let courses: CourseInfo[] = [];
+  
+  // Funktion som laddar kurser från localStorage (om det finns några sparade)
+  function loadCourses(): void {
+    const stored = localStorage.getItem('courses');
+    if (stored) {
+      courses = JSON.parse(stored);
+      renderCourses();
+    }
   }
   
